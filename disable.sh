9397347fe6244c9929c684559eba8b9ee85f2719
wikadm <app-front@PROD-ansible.esa.int>
2024-05-31 10:05:33 +0200

    description added

--- disable.sh
+++ disable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+##Script which diasble inactive user from file *_api.out
 
 xargs -I {} curl -v -D- -u 'user:password' -X PUT --data '{"username": "{}", "active": false }'  -H "Content-Type: application/json" https://yourjiraurl/rest/api/2/user?username={} <  > /data/log/`date +%Y%m`_api.out
 
