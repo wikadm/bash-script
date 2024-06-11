37f54875920f7d88d3b80e8f4fd939c5e30e0d9f
wikadm <app-front@PROD-ansible.esa.int>
2024-06-11 09:49:03 +0200

    Install alert exporter for prometheus

--- alert_exporter.sh
+++ alert_exporter.sh
@@ -1,4 +1,4 @@
-cd /home/app-front
+cd /home/user
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
 
 tar -xvzf node_exporter-1.8.1.linux-amd64.tar.gz
